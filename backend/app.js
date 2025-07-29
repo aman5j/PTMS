c3eac90ac23dfbc135bc0c134400b70bc74a6832
aman5j <amanprajapati190@gmail.com>
2025-07-29 17:57:29 +0530

    Changes on app.js file

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
 // const store = MongoStore.create({
     // mongoUrl: dbUrl,   //MONGO_URL
     // crypto: {
