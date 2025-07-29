c47f7323de29146a352909262b7b9a4955214687
aman5j <amanprajapati190@gmail.com>
2025-07-29 20:32:58 +0530

    .env and FetchNodeServices Update

--- frontend/src/services/FetchNodeServices.jsx
+++ frontend/src/services/FetchNodeServices.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 // import.meta.env;
 
 var serverURL=import.meta.env.VITE_BASE_URL;
+// const baseUrl = import.meta.env.VITE_BASE_URL;
+
 
 const getData = async(url) => {
     try {
